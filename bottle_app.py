580765069cd0ac77cea37f4801dc5231b534b735
manasaperavali <peravalimanasa15@gmail.com>
2022-10-25 16:27:01 +0000

    first set of files

--- bottle_app.py
+++ bottle_app.py
@@ -5,6 +5,9 @@ from bottle import default_app, route
 @route('/')
 def hello_world():
     return 'Hello from Bottle!'
+@route('/bye')
+def bye_world():
+    return 'Bye from Bottle!'
 
 application = default_app()
 
